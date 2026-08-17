4788890c56b84c1bdfa154c1204f34bef04650b4
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2026-08-18 03:11:09 +0530

    perf: make hover state change listeners passive

--- apps/web/rllm/src/directives/hover-state-change.ts
+++ apps/web/rllm/src/directives/hover-state-change.ts
@@ -4,10 +4,14 @@ import { createEventListenerMap } from '@solid-primitives/event-listener';
 
 export function hoverStateChange(ref: HTMLElement, options: Accessor<(value: boolean) => void>) {
   const onChange = options();
-  createEventListenerMap(ref, {
-    focusin: () => onChange(true),
-    focusout: () => onChange(false),
-    mouseenter: () => onChange(true),
-    mouseleave: () => onChange(false)
-  });
+  createEventListenerMap(
+    ref,
+    {
+      focusin: () => onChange(true),
+      focusout: () => onChange(false),
+      mouseenter: () => onChange(true),
+      mouseleave: () => onChange(false)
+    },
+    { passive: true }
+  );
 }
