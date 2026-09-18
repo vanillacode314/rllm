a9f36f350eb8b7f24b80dffb7bee137c65778697
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2026-09-19 01:02:38 +0530

    chore: remove legacy typescript sync-server

--- apps/sync-server/oxfmt.config.ts
+++
@@ -1,6 +0,0 @@
-import config from '@rthings/config/oxfmt.config';
-import { defineConfig } from 'oxfmt';
-
-export default defineConfig({
-  ...config
-});
--- apps/sync-server/src/utils/auth.ts
+++
@@ -1,6 +0,0 @@
-import { ethers } from 'ethers';
-
-export function verifyData(data: string | Uint8Array, signature: string, accountId: string) {
-  const recoveredAccountId = ethers.verifyMessage(data, signature);
-  return recoveredAccountId === accountId;
-}
